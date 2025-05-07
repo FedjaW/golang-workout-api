d6be973bb012331ff6b23328579ac19ed457254c
FedjaW <f.wilms5189@gmail.com>
2025-05-07 19:43:17 +0200

    used chi to handle routes

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/fedaw/golang-workout-api/internal/app"
+	"github.com/fedaw/golang-workout-api/internal/routes"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
-	app.Logger.Println("app up and running...")
 
+	r := routes.SetupRoutes(app)
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	app.Logger.Printf("listening on port %d\n", port)
 
 	err = server.ListenAndServe()
 	if err != nil {
