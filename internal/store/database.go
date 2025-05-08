14473d078624d9f6bee14efd5524f9a4d272f410
FedjaW <f.wilms5189@gmail.com>
2025-05-08 10:17:44 +0200

    connected to postgres

---
+++ internal/store/database.go
@@ -0,0 +1,19 @@
+package store
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func Open() (*sql.DB, error) {
+	db, err := sql.Open("pgx", "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable")
+	if err != nil {
+		return nil, fmt.Errorf("d: open %w", err)
+	}
+
+	fmt.Println("successfully connected to database...")
+
+	return db, nil
+}
